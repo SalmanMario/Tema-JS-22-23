3e5df3275105e7e92dbebc19e13b88316c111f49
Mario <xmariosalman@gmail.com>
2023-02-22 03:06:19 +0200

    schimbat js IARA LA 3 DIMINEATA

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// tema partial completata,mai trebuie niste fix-uri pe ici,colo
+
 fetch('https://jsonplaceholder.typicode.com/posts')
     .then((response) => {
         return response.text();
@@ -6,48 +8,48 @@ fetch('https://jsonplaceholder.typicode.com/posts')
         const parsedResult = JSON.parse(unparsedResult);
         const root = document.querySelector("#root");
 
+        function displayData() {
+            root.innerHTML = parsedResult.map((elem) => {
+                return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
+            <p> iar descrierea este: ${elem.body} </p>
+            <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
+            }).join("");
+        }
+
         if (localStorage.getItem("key") === null) {
             setTimeout(() => {
-                root.innerHTML = parsedResult.map((elem) => {
-                    return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
-                <p> iar descrierea este: ${elem.body} </p>
-                <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
-                }).join("");
+                displayData();
             }, 3000);
 
         } else {
-            root.innerHTML = parsedResult.map((elem) => {
-                return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
-                <p> iar descrierea este: ${elem.body} </p>
-                <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
-            }).join("");
+            displayData();
         }
 
         localStorage.setItem("key", JSON.stringify(parsedResult));
         JSON.parse(localStorage.getItem("key", parsedResult));
 
-        // root.innerHTML = parsedResult.map((elem) => {
-        //     return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
-        //         <p> iar descrierea este: ${elem.body} </p>
-        //         <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
-        // }).join("");
-
         console.log(parsedResult);
 
+        // am adaugat aici un timeout deoarece daca nu avem salvat nimic in localStorage
+        // nu putem sterge postarea fara a da un refresh paginii
+
+        // puii mei cu functiile asincrone ii 3 dimineata,merg sa ma culc
+
         parsedResult.forEach(elem => {
+            setTimeout(() => {
+                const button = document.querySelector(`#deleteButton${elem.id}`);
+                button.addEventListener("click", function () {
+                    fetch('https://jsonplaceholder.typicode.com/posts/' + elem.id, {
+                        method: 'DELETE',
+                    })
+                        .then(res => res.text()) // or res.json()
+                        .then(res => console.log(res));
+                    console.log(elem);
 
-            const button = document.querySelector(`#deleteButton${elem.id}`);
-            button.addEventListener("click", function () {
-                fetch('https://jsonplaceholder.typicode.com/posts/' + elem.id, {
-                    method: 'DELETE',
-                })
-                    .then(res => res.text()) // or res.json()
-                    .then(res => console.log(res));
-                console.log(elem);
-
-                const deletedDiv = document.querySelector(`#element${elem.id}`);
-                deletedDiv.remove();
-            });
+                    const deletedDiv = document.querySelector(`#element${elem.id}`);
+                    deletedDiv.remove();
+                });
+            }, 4000);
         });
 
     });
