ac1697fdfe14947c6bd4e5478d64ee4d789fb3af
Mario <xmariosalman@gmail.com>
2023-02-27 10:10:50 +0200

    tema alex

--- js/script.js
+++ js/script.js
@@ -1,84 +1,106 @@
-// tema partial completata,mai trebuie niste fix-uri pe ici,colo
 
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => {
-        return response.text();
-    })
-    .then((unparsedResult) => {
-        const parsedResult = JSON.parse(unparsedResult);
-        const root = document.querySelector("#root");
+function loadStorageData() {
+    const posts = localStorage.getItem("posts");
+    if (!posts) {
+        return [];
+    }
+    const parsedPosts = JSON.parse(posts);
 
-        function displayData() {
-            root.innerHTML = parsedResult.map((elem) => {
-                return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
-            <p> iar descrierea este: ${elem.body} </p>
-            <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
-            }).join("");
-        }
+    return parsedPosts;
+}
 
-        if (localStorage.getItem("key") === null) {
-            setTimeout(() => {
-                displayData();
-            }, 3000);
+let posts = loadStorageData();
+displayPosts();
 
-        } else {
-            displayData();
-        }
-
-        localStorage.setItem("key", JSON.stringify(parsedResult));
-        JSON.parse(localStorage.getItem("key", parsedResult));
-
-        console.log(parsedResult);
-
-        // am adaugat aici un timeout deoarece daca nu avem salvat nimic in localStorage
-        // nu putem sterge postarea fara a da un refresh paginii
-
-        // puii mei cu functiile asincrone ii 3 dimineata,merg sa ma culc
-
-        parsedResult.forEach(elem => {
-            setTimeout(() => {
-                const button = document.querySelector(`#deleteButton${elem.id}`);
-                button.addEventListener("click", function () {
-                    fetch('https://jsonplaceholder.typicode.com/posts/' + elem.id, {
-                        method: 'DELETE',
-                    })
-                        .then(res => res.text()) // or res.json()
-                        .then(res => console.log(res));
-                    console.log(elem);
-
-                    const deletedDiv = document.querySelector(`#element${elem.id}`);
-                    deletedDiv.remove();
-                });
-            }, 4000);
+function deletePost(id) {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, { method: "DELETE" })
+        .then(() => {
+            posts = posts.filter(post => post.id !== id);
+            displayPosts();
         });
+}
 
+function displayPosts() {
+    const root = document.querySelector("#root");
+    root.innerHTML = `
+        ${posts.map(post => `
+        <div class="post">
+            <h2>${post.title}</h2>
+            <h3>Post ID: ${post.id}</h3>
+            <p>${post.body}</p>
+            <button id="delete-${post.id}">Delete</button>
+        </div>
+        `).join("")}
+    `;
+    posts.forEach(post => {
+        const button = document.querySelector(`#delete-${post.id}`);
+        button.addEventListener("click", function () {
+            deletePost(post.id);
+        });
+    });
+}
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        posts = data;
+        localStorage.setItem("posts", JSON.stringify(data));
+        displayPosts();
     });
 
 
-/* bug la localStorage,daca pun if-ul de mai jos parsedResult.forEach((elem) => {
+// tema mea
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then((response) => {
+//         return response.text();
+//     })
+//     .then((unparsedResult) => {
+//         const parsedResult = JSON.parse(unparsedResult);
+//         const root = document.querySelector("#root");
 
-    nu mai pot sterge descrierea si titlu si nici nu-mi apare apare eroare in console
-    cumva button-ul cred ca este null si nu stiu cum sa-l rezolv
-})
+//         function displayData() {
+//             root.innerHTML = parsedResult.map((elem) => {
+//                 return `<div id=element${elem.id}> <h2> Titlul elementelor este: ${elem.title} </h2>
+//             <p> iar descrierea este: ${elem.body} </p>
+//             <button id="deleteButton${elem.id}">Delete Post</button> </div>`;
+//             }).join("");
+//         }
 
-*/
+//         if (localStorage.getItem("key") === null) {
+//             setTimeout(() => {
+//                 displayData();
+//             }, 3000);
 
- // if (localStorage.getItem("key") === null) {
-    //     setTimeout(() => {
-    //         root.innerHTML = parsedResult.map((elem) => {
-    //             return `<div id=element${elem.id}></div> <h2> Titlul elementelor este: ${elem.title} </h2>
-    //             <p> iar descrierea este: ${elem.body} </p>
-    //             <button id="deleteButton${elem.id}">Delete Post</button>`;
-    //         }).join("");
+//         } else {
+//             displayData();
+//         }
 
-    //         localStorage.setItem("key", JSON.stringify(elem.id));
+//         localStorage.setItem("key", JSON.stringify(parsedResult));
+//         JSON.parse(localStorage.getItem("key", parsedResult));
 
-    //     }, 3000);
+//         console.log(parsedResult);
 
-    // } else {
-    //     root.innerHTML = parsedResult.map((elem) => {
-    //         return `<div id=elementButton${elem.id}></div> <h2> Titlul elementelor este: ${elem.title} </h2>
-    //             <p> iar descrierea este: ${elem.body} </p>
-    //             <button id="deleteButton${elem.id}">Delete Post</button>`;
-    //     }).join("");
-    // }
+//         // am adaugat aici un timeout deoarece daca nu avem salvat nimic in localStorage
+//         // nu putem sterge postarea fara a da un refresh paginii
+
+//         // puii mei cu functiile asincrone ii 3 dimineata,merg sa ma culc
+
+//         parsedResult.forEach(elem => {
+//             setTimeout(() => {
+//                 const button = document.querySelector(`#deleteButton${elem.id}`);
+//                 button.addEventListener("click", function () {
+//                     fetch('https://jsonplaceholder.typicode.com/posts/' + elem.id, {
+//                         method: 'DELETE',
+//                     })
+//                         .then(res => res.text()) // or res.json()
+//                         .then(res => console.log(res));
+//                     console.log(elem);
+
+//                     const deletedDiv = document.querySelector(`#element${elem.id}`);
+//                     deletedDiv.remove();
+//                 });
+//             }, 4000);
+//         });
+
+//     });
